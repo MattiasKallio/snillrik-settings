58cd57624af1f833fea530f25dd4b87e46dcd81a
Mattias Kallio <kallio@snillrik.se>
2021-09-20 13:39:58 +0200

    Give me some space!

--- classes/turnoffxmlrpc.php
+++ classes/turnoffxmlrpc.php
@@ -8,7 +8,7 @@ class SNSET_TurnOffXMLRPC
     {
         //probably using soon.
         $turnoffxmlrpc = get_option('snillrik_settings_turnoffxmlrpc', array());
-
+        
         if ($turnoffxmlrpc == "on") {
             add_filter('xmlrpc_enabled', '__return_false');
         }
