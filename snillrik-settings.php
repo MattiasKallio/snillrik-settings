8e898a70fad5eff1a653d7e11ca0878ecf335e32
Mattias Kallio <kallio@snillrik.se>
2023-11-08 15:45:05 +0100

    Update snillrik-settings.php

--- snillrik-settings.php
+++ snillrik-settings.php
@@ -3,7 +3,7 @@
 Plugin Name: Snillrik Settings
 Plugin URI: http://www.snillrik.se/
 Description: Snillrik settings is a plugin for som basic settings like turning of Gutenberg, adding css, turning of comments etc.
-Version: 1.1.5
+Version: 1.1.6
 Author: Mattias Kallio
 Author URI: http://www.snillrik.se
 License: GPL2
